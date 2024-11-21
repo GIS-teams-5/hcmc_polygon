84549545458eb46fc8aa79d2c41e9df987721923
Bird <tieuvohonghan@gmail.com>
2024-11-22 00:07:28 +0700

    Add images and icon

--- PublicPoints.js
+++ PublicPoints.js
@@ -8,6 +8,9 @@ function createThreePublicPoint(Point, Graphic) {
       address:
         "8 Đ. Võ Văn Kiệt, Phường Nguyễn Thái Bình, Quận 1, Hồ Chí Minh 70000, Vietnam",
       phoneNumber: "02838213957",
+      url: "https://napsg-web.s3.amazonaws.com/symbology/data/PNG9/infrastructure/CRTINS_FINANC/FDIC_InsuredBanks_256x256.png",
+      image:
+        "https://cafefcdn.com/thumb_w/640/203337114487263232/2023/11/22/nhnn-17006602955151100543589-0-59-769-1289-crop-1700660304811454439439.png",
     },
     {
       name: "Military Hospital 175",
@@ -15,13 +18,19 @@ function createThreePublicPoint(Point, Graphic) {
       latitude: 10.819904122762964,
       address: "786 Đ. Nguyễn Kiệm, Phường 3, Gò Vấp, Hồ Chí Minh, Vietnam",
       phoneNumber: "19001175",
+      url: "https://napsg-web.s3.amazonaws.com/symbology/data/PNG9/lifelines/Health_and_Medical/health-and-medical-with-label-red-halo_256x256.png",
+      image:
+        "https://data-service.pharmacity.io/pmc-upload-media/production/pmc-ecm-asm/blog/benh-vien-quan-y-115.webp",
     },
     {
-      name: "Ho Chi Minh City Post Office",
-      longitude: 106.6999905540011,
-      latitude: 10.779943623910858,
-      address: "125 Hai Bà Trưng, Bến Nghé, Quận 1, Hồ Chí Minh, Vietnam",
-      phoneNumber: "02839247247",
+      name: "Tan Son Nhat International Airport",
+      longitude: 106.66407794639869,
+      latitude: 10.816075698832357,
+      address: "Đ. Trường Sơn, Phường 2, Tân Bình, Hồ Chí Minh, Vietnam",
+      phoneNumber: "02838485383",
+      url: "https://napsg-web.s3.amazonaws.com/symbology/data/PNG9/lifelines/Transportation/transportation_aviation-green-halo_256x256.png",
+      image:
+        "https://sbay.com.vn/upload/editor/%E1%BA%A2nh%20%20%C4%91%C3%A0o%20t%E1%BA%A1o%20h%E1%BB%87%20th%E1%BB%91ng/tan-son-nhat.jpg",
     },
   ];
   console.log(publicPoints);
@@ -31,25 +40,12 @@ function createThreePublicPoint(Point, Graphic) {
       longitude: publicPoint.longitude,
       latitude: publicPoint.latitude,
     });
-    // const symbol = {
-    //   type: "text", // autocasts as new TextSymbol()
-    //   color: "white",
-    //   haloColor: "blue",
-    //   haloSize: 1,
-    //   text: publicPoint.name,
-    //   yoffset: 20,
-    //   xoffset: 30,
-    //   font: {
-    //     size: 15,
-    //   },
-    //   width: 1,
-    // };
 
     var symbol = {
       type: "picture-marker", // autocasts as new PictureMarkerSymbol()
-      url: "https://static.arcgis.com/images/Symbols/Shapes/Bank.png",
-      width: "64px",
-      height: "64px",
+      url: publicPoint.url,
+      width: "32px",
+      height: "32px",
     };
 
     // create attributes
@@ -57,12 +53,13 @@ function createThreePublicPoint(Point, Graphic) {
       Name: publicPoint.name,
       Location: publicPoint.address,
       PhoneNumber: publicPoint.phoneNumber,
+      Image: publicPoint.image,
     };
     // Create popup template
     const popupTemplate = {
       title: publicPoint.name,
       content:
-        "<b>Name</b> located at <b>{Location}</b>. <br> Phone Number: <b>{PhoneNumber}</b>",
+        "<div class='popup-content'><img src='{Image}' height='200px' alt='image description'></div> <br> <b>{Name}</b> located at <b>{Location}</b>. <br> Phone Number: <b>{PhoneNumber}</b>",
     };
 
     const pointGraphic = new Graphic({
